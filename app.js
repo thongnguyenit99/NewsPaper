7143aa49207636347139b7d3436b8480535156a9
thongnguyen <you@example.com>
2020-06-12 11:03:15 +0700

    add file app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const exphbs = require('express-handlebars');
+
+app.use(express.urlencoded({ extended: true }));
+app.engine('hbs', exphbs({
+    defaultLayout: 'main.hbs'
+}))
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+app.get('/about.html', (req, res) => {
+    res.render('about');
+})
+app.get('/contact.html', (req, res) => {
+    res.render('contact');
+})
+
+app.use(function (req, res) {
+    res.render('404');
+})
+
+app.listen(port, () => {
+    console.log(`Server start port at http://localhost:${port}`);
+})
