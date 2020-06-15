e7d8612c9208a4ebc87de573ba3cb89068075641
thongnguyen <you@example.com>
2020-06-15 18:38:19 +0700

    upload db: news.sql

---
+++ public/news.sql
@@ -0,0 +1,252 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 12, 2020 at 11:02 AM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.3.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `news`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `account`
+--
+
+CREATE TABLE `account` (
+  `a_ID` int(11) NOT NULL,
+  `tc_ID` int(11) DEFAULT NULL,
+  `username` varchar(64) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `a_Name` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `cre_Date` date DEFAULT NULL,
+  `r_ID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `account`
+--
+
+INSERT INTO `account` (`a_ID`, `tc_ID`, `username`, `password`, `a_Name`, `cre_Date`, `r_ID`) VALUES
+(1, 3, 'thongnguyen', '$2y$10$ib/i1jHiZ5lTHYXTsWwhXOi5E.fpCPcY7KvREYsG6hmCijOWcbw7K', 'Nguyễn Văn Thông', '2020-06-10', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `c_ID` int(3) NOT NULL,
+  `tc_ID` int(11) DEFAULT NULL,
+  `c_Name` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `c_Large` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`c_ID`, `tc_ID`, `c_Name`, `c_Large`) VALUES
+(1, 1, 'Cổ Phiếu Top Đầu', 'Chứng Khoán'),
+(2, 1, 'Xu Hướng Nhận Định', 'Chứng Khoán'),
+(3, 2, 'Bất Động Sản', 'Doanh Nghiệp'),
+(4, 2, 'Doanh Nghiệp Niêm Yết', 'Doanh Nghiệp'),
+(5, 3, 'Ngân Hàng Điện Tử', 'Tài Chính'),
+(6, 3, 'Thương Mại Điện Tử', 'Tài Chính');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `p_ID` int(3) NOT NULL,
+  `tc_ID` int(11) DEFAULT NULL,
+  `tag` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `title` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `abstract` text COLLATE utf8_unicode_ci,
+  `content` text COLLATE utf8_unicode_ci,
+  `author` varchar(64) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `public_date` date DEFAULT NULL,
+  `images` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `status` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role_account`
+--
+
+CREATE TABLE `role_account` (
+  `ID` int(11) NOT NULL,
+  `Name` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `role_account`
+--
+
+INSERT INTO `role_account` (`ID`, `Name`) VALUES
+(1, 'Độc giả'),
+(2, 'Phóng viên'),
+(3, 'Biên tập viên'),
+(4, 'Quản trị viên');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `social_provider`
+--
+
+CREATE TABLE `social_provider` (
+  `ID` int(11) NOT NULL,
+  `provider_id` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `provider` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'tên các mxh mà mình login',
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tag`
+--
+
+CREATE TABLE `tag` (
+  `ID` int(11) NOT NULL,
+  `Name` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `type_catelgories`
+--
+
+CREATE TABLE `type_catelgories` (
+  `tc_ID` int(3) NOT NULL,
+  `tc_Name` varchar(150) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `type_catelgories`
+--
+
+INSERT INTO `type_catelgories` (`tc_ID`, `tc_Name`) VALUES
+(1, 'Chứng Khoán'),
+(2, 'Doanh Nghiệp'),
+(3, 'Tài Chính');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `account`
+--
+ALTER TABLE `account`
+  ADD PRIMARY KEY (`a_ID`);
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`c_ID`);
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`p_ID`);
+
+--
+-- Indexes for table `role_account`
+--
+ALTER TABLE `role_account`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `social_provider`
+--
+ALTER TABLE `social_provider`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tag`
+--
+ALTER TABLE `tag`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `type_catelgories`
+--
+ALTER TABLE `type_catelgories`
+  ADD PRIMARY KEY (`tc_ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `account`
+--
+ALTER TABLE `account`
+  MODIFY `a_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `c_ID` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `p_ID` int(3) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `role_account`
+--
+ALTER TABLE `role_account`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `social_provider`
+--
+ALTER TABLE `social_provider`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tag`
+--
+ALTER TABLE `tag`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `type_catelgories`
+--
+ALTER TABLE `type_catelgories`
+  MODIFY `tc_ID` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
