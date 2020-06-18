dddcd875cf9e7d696a44f31ca7bfb46bd58c96fe
thongnguyen <you@example.com>
2020-06-18 11:11:03 +0700

    add featured article function

--- models/article.model.js
+++ models/article.model.js
@@ -5,8 +5,13 @@ module.exports = {
     all: function () {
         return db.load(`select * from ${TBL_article}`);
     },
+    // 10 newest article
     newest: function () {
-        return db.load(`select * from ${TBL_article} a join categories c on a.c_ID=c.ID order by public_date DESC limit 10`);
+        return db.load(`select * from ${TBL_article} a join categories c on a.c_ID=c.ID where isActive=1 order by public_date DESC limit 10`);
+    },
+    // 10 best featured article
+    bestnew: function () {
+        return db.load(`select * from ${TBL_article} a join categories c on a.c_ID=c.ID where isActive=1 order by featured limit 10`);
     }
 
 }
