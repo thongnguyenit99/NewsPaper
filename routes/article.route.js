f2f72fa6e22b8f1cc44b8ee193f9e6939295a4a1
thongnguyen <you@example.com>
2020-06-25 16:30:06 +0700

    update comment

--- routes/article.route.js
+++ routes/article.route.js
@@ -4,6 +4,8 @@ const catModel = require('../models/category.model');
 const comModel = require('../models/comment.model');
 const acc_roleModel = require('../models/account_role.model');
 const accountModel = require('../models/_account.model.js');
+const restrict = require("../middlewares/auth.mdw");
+
 
 const moment = require('moment');
 const router = express.Router();
@@ -21,25 +23,27 @@ router.get('/list', async (req, res) => {
     });
 })
 
-router.get('/:c_alias/:title', async function (req, res) {
+router.get('/:c_alias/:id/:title', async function (req, res) {
     const today = moment().format('YYYY-MM-DD'); // lấy ngày hiện tại
-       
+    //const id_article = req.params.id_article;   
     const title = req.params.title;
     const nameChildCat = req.params.c_alias;
-   
+    const id = req.params.id;
+    var user = req.session.authUser;
+    //console.log(user);
 
-    var user = await accountModel.singleByUserName(req.body.username);
-    if (user.length > 0) {
-        req.session.isAuthenticated = true;
-        req.session.authUser = user[0];
-    }
-    
-    const list = await articleModel.detailByTitle(title);
-    const list5Art_same = await articleModel.ArtSameCat(nameChildCat);
-
+    // bỏ vào đây chạy song song
+    const [list, list5Art_same, opinion, get] = await Promise.all([
+        articleModel.detailByTitle(title),
+        articleModel.ArtSameCat(nameChildCat),
+        comModel.getByArticle(title),
+        comModel.getId_article(id)
+    ]);
     res.render('vwArticle/details', {
         list,
         list5Art_same,
+        opinion,
+        get,
         helpers: {
             format_DOB: function (date) {
                 return moment(date, 'YYYY/MM/DD').format('DD-MM-YYYY');
@@ -47,7 +51,6 @@ router.get('/:c_alias/:title', async function (req, res) {
             splitTitle: function (tag) {
                 for (var i = 0; i < tag.length; i++) {
                     var t = tag.split(';');
-                   
                     return t[0];
                 }
             },
@@ -66,5 +69,63 @@ router.get('/:c_alias/:title', async function (req, res) {
         }
     });
 })
+router.post('/:c_alias/:id/:title',restrict ,async (req, res) => {
+    const title = req.params.title;
+    const nameChildCat = req.params.c_alias;
+    const id = req.params.id;
+    const today = moment().format('YYYY-MM-DD'); // lấy ngày hiện tại
+    //var user = req.session.authUser;
+    //console.log(user);
+
+    // nếu tồn tại user đã đăng nhập và quyền là độc giả
+    if (req.session.authUser.r_ID == 1) {
+             var obj = {
+            readerName: req.body.readerName,
+            ID_Account: req.session.authUser.ID,
+            ID_Article: id,
+            Content: req.body.Content,
+            created_at: today
+        }
+        // bỏ vào đây chạy song song
+        const [add, list, list5Art_same, opinion,get] = await Promise.all([
+            comModel.insertComment(obj),
+            articleModel.detailByTitle(title),
+            articleModel.ArtSameCat(nameChildCat),
+            comModel.getByArticle(title),
+            comModel.getId_article(id)
+        ]);
+        console.log(obj);
+        res.render('vwArticle/details', {
+            add, list, list5Art_same, opinion, get,
+            helpers: {
+                format_DOB: function (date) {
+                    return moment(date, 'YYYY/MM/DD').format('DD-MM-YYYY');
+                },
+                splitTitle: function (tag) {
+                    for (var i = 0; i < tag.length; i++) {
+                        var t = tag.split(';');
+                        return t[0];
+                    }
+                },
+                splitTitle1: function (tag) {
+                    for (var i = 0; i < tag.length; i++) {
+                        var t = tag.split(';');
+                        return t[1];
+                    }
+                },
+                splitTitle2: function (tag) {
+                    for (var i = 0; i < tag.length; i++) {
+                        var t = tag.split(';');
+                        return t[2];
+                    }
+                }
+            }});
+    }
+    else {
+        console.log('bạn phải đăng nhập');
+    }
+
+
+});
 
 module.exports = router;
