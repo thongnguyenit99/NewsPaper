1a596104b3604e365f489e40258bc5597e836013
thongnguyenit99 <nvthong.it@gmail.com>
2020-07-13 02:48:57 +0700

    hoàn thành quản lý chuyên mục:crud cha và con

--- routes/Admin.route.js
+++ routes/Admin.route.js
@@ -8,11 +8,19 @@ const moment = require('moment');
 var fs = require('fs');
 const mkdirp = require('mkdirp');
 const router = express.Router();
-router.get('/', restrict, restrictadmin, (req, res) => {
-     //var user = req.session.authUser;
+const multer = require('multer');
+const storage = multer.diskStorage({
+     filename(req, file, cb) {
+          cb(null, file.originalname);
+     },
+     destination(req, file, cb) {
+          cb(null, './public/admin/img/');
+     }
+})
+const upload = multer({ storage });
 
-     // console.log(list);
-     //user.r_ID === 4 && user !== 'undefined' && user !== null ?
+
+router.get('/', restrict, restrictadmin, (req, res) => {
       res.render('vwAccount/vwAdvantage/admin/home', { layout: 'mainAdmin.hbs' }) //: res.render('403');
 });
 
@@ -21,13 +29,6 @@ router.get('/logout', restrict, async function (req, res) {
      req.session.authUser = null;
      res.redirect(req.headers.referer);
 })
-
-router.get('/tag', restrict, (req, res) => {
-     var user = req.session.authUser;
-     user.r_ID === 4 && user !== "undefined" && user !== null && user.r_ID !== null && user.r_ID !== "undefined" ? res.render('vwAccount/vwAdvantage/admin/tag/list', { layout: 'mainAdmin.hbs' }) : res.render('home');
-});
-
-
 
 
 // quản lý chuyên mục //
@@ -60,7 +61,13 @@ router.get('/categories/add', restrict, async (req, res) => {
           res.render('403');
      }
 });
-// Hiển thị chyên mục con theo chuyên mục cha
+// hiện trang thêm chuyên mục cha
+router.get('/categories/addCat', restrict,restrictadmin, async (req, res) => {
+          const [typeCat] = await Promise.all([
+               tpCatModel.getAll()]);
+          res.render('vwAccount/vwAdvantage/admin/categories/addCat', { typeCat, layout: 'mainAdmin.hbs' })
+});
+// Hiển thị chyên mục con theo chuyên mục cha (chi tiết chuyên mục cha)
 router.get('/categories/:alias', restrict, async function (req, res) {
      var user = req.session.authUser;
      if (user.r_ID === 4 && user !== "undefined" && user !== null && user.r_ID !== null && user.r_ID !== "undefined") {
@@ -82,6 +89,18 @@ router.get('/categories/:alias', restrict, async function (req, res) {
 
 })
 // chỉnh sửa chuyên mục theo id
+router.get('/categories/editCat/:id', restrict, restrictadmin,async (req, res) => {
+          const id = +req.params.id || -1;
+     const rows = await tpCatModel.singleCat(id);
+          if (rows.length === 0)
+               return res.send('Biến không có giá trị.');
+          const cats = rows[0];
+
+          res.render('vwAccount/vwAdvantage/admin/categories/editCat', {
+               cats, layout: 'mainAdmin.hbs'
+          })
+});
+// chỉnh sửa chuyên mục con theo id
 router.get('/categories/edit/:id', restrict, async (req, res) => {
      var user = req.session.authUser;
 
@@ -116,12 +135,10 @@ router.get('/categories/details/:conId', restrict, async function (req, res) {
      }
 
 })
-// xử lý thêm chuyên mục
-router.post('/categories/add', restrict, async function (req, res) {
-     var user = req.session.authUser;
-     if (user.r_ID === 4 && user !== "undefined" && user !== null && user.r_ID !== null && user.r_ID !== "undefined")
-     {
-          var row = await catModel.getByCatId(req.body.tc_ID);
+// xử lý thêm chuyên mục con
+router.post('/categories/add', upload.single('c_images'), restrictadmin, restrict, async function (req, res) {
+     if (req.file) {
+     var row = await catModel.getByCatId(req.body.tc_ID);
           var path = row[0].path;
           path = path.split('/');
           path = path[0] + "/" + req.body.c_Name + "/";
@@ -139,65 +156,92 @@ router.post('/categories/add', restrict, async function (req, res) {
           }
           else{
                c_Large ="Kiến Thức Đầu Tư";
-          }
+     }
           var entity = {
-               ...req.body, c_Large,}
+               ...req.body,
+               c_Large,
+               c_images: req.file.filename,
+               c_isActive:1
+          }
                const addCat = await catModel.insertCat(entity);
                res.redirect('/admin/categories/add');
                //console.log(addCat);
      }
      else {
-          res.render('403');
+          res.render('500')
      }
-
 })
-// xử lý xóa chuyên mục theo id
-router.post('/categories/del', restrict, async function (req, res) {
-     var user = req.session.authUser;
-     if (user.r_ID === 4 && user !== "undefined" && user !== null && user.r_ID !== null && user.r_ID !== "undefined") {
-          await catModel.delCat(req.body.ID);
-          res.redirect('/admin/categories');
+// xử lý xóa chuyên mục con theo id
+// router.post('/categories/del', restrict, async function (req, res) {
+//      var user = req.session.authUser;
+//      if (user.r_ID === 4 && user !== "undefined" && user !== null && user.r_ID !== null && user.r_ID !== "undefined") {
+//           await catModel.delCat(req.body.ID);
+//           res.redirect('/admin/categories');
+//      }
+//      else {
+//           res.render('403');
+//      }
+
+// })
+// xử lý xóa chuyên mục con theo id
+router.post('/categories/del', restrict, restrictadmin, async (req, res) => {
+     if (req.body.ID != "") {
+          await catModel.delChild({ ID: req.body.ID });
      }
-     else {
-          res.render('403');
-     }
+     res.redirect('/admin/categories');
+});
 
-})
-// xử lý cập nhật chuyên mục
-router.post('/categories/update', restrict, async function (req, res) {
-     var user = req.session.authUser;
-     if (user.r_ID === 4 && user !== "undefined" && user !== null && user.r_ID !== null && user.r_ID !== "undefined") {
-          if (req.body.c_Name == '' || req.body.c_alias == '' || req.body.c_Name == '' && req.body.c_alias == '') {
-
-               if (req.body.c_Name == '')
-               {
-                    res.render('vwAccount/vwAdvantage/admin/categories/error-alert', {
-                         layout: 'mainAdmin.hbs',
-                         err: 'Tên chuyên mục không được rỗng!Mời bạn nhập lại!!',
-                         idCat: req.body.ID
-                    })
-               }
-               else if ( req.body.c_alias == '')
-               {
-                    res.render('vwAccount/vwAdvantage/admin/categories/error-alert', {
-                         layout: 'mainAdmin.hbs',
-                         err: 'Bí danh không được rỗng!!Mời bạn nhập lại!',
-                         idCat:req.body.ID,
-                    })
-               }
-          }
-          else {
-
-               await catModel.updateCat(req.body);
-               console.log(req.body);
+// xử lý cập nhật chuyên mục con
+router.post('/categories/update', upload.single('c_images'), restrictadmin, restrict, async function (req, res) {
+     if (req.file) {
+               var entity = {
+                    ...req.body,
+                    c_images:req.file.filename
+              }
+               await catModel.updateCat(entity);
+               console.log(entity);
                res.redirect('/admin/categories');
           }
-     }
      else {
-          res.render('403');
+          res.render('500');
      }
 })
+// xử  lý thêm chuyên mục cha
+router.post('/categories/addCat', upload.single('images'),  restrict, restrictadmin, async function (req, res) {
+     if (req.file) {
+         // console.log(req.body);
+          var entity = {
+               ...req.body,
+               images: req.file.filename,
+               tc_isActive: 1
+          }
+          const addCat = await tpCatModel.insertCat(entity);
+          res.redirect('/admin/categories/addCat');
+         // console.log(entity)
+     }
+})
+// xử lý xóa chuyên mục cha theo id
+router.post('/categories/delCat', restrict, restrictadmin, async function (req, res) {
+     if (req.body.ID != "") {
+          await tpCatModel.delCat({ ID: req.body.ID });
+     }
+     res.redirect('/admin/categories');
 
+})
 
-
+// cập nhật chuyên mục cha
+router.post('/categories/updateCat', upload.single('images'), restrictadmin, restrict, async function (req, res) {
+     if (req.file) {
+               var entity = {
+                    ...req.body,
+                    images: req.file.filename
+               }
+               await tpCatModel.updateCat(entity);
+               console.log(entity);
+               res.redirect('/admin/categories');
+     }
+     else {
+          res.render('500');
+     }
+})
 module.exports = router;
