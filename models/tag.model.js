26bad89cc6d1382711c917cc0645be250f341bd4
thongnguyenit99 <nvthong.it@gmail.com>
2020-07-14 13:46:26 +0700

    xong crud: quản lý tag

--- models/tag.model.js
+++ models/tag.model.js
@@ -14,7 +14,25 @@ module.exports = {
         WHERE ta.id_article=a.id and a.sts_id=2 and t.ID=ta.id_tag and t.Name = '${name}'`);
         return rows[0].total;
     },
+    detailById: (id) => {
+        return db.load(`SELECT DISTINCT t.ID,t.Name,t.tg_alias,a.id,a.title,a.sts_id,a.images,a.public_date,a.isActive,ta.id_article,ta.id_tag 
+FROM tag t join tag_article ta on t.ID=ta.id_tag JOIN article a on ta.id_article=a.id
+ WHERE t.ID=${id} and a.sts_id=2 and a.isActive=1`);
+    },
     delTag: function (id) {
         return db.del(`delete from ${TBL_tag} where ?`, id);
+    },
+    addTag: function (entity) {
+        return db.insert(TBL_tag, entity);
+    },
+    updateTag: function (entity) {
+        const condition = {
+            ID: entity.ID
+        }
+        delete entity.ID;
+        return db.update(TBL_tag, entity, condition);
+    },
+    single: function (Id) {
+        return db.load(`select * from ${TBL_tag} where ID = ${Id}`);
     },
 }
