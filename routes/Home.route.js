be09cca1e89976f09ad27e7a33c0ec7f72f4d93d
thongnguyenit99 <nvthong.it@gmail.com>
2020-07-14 11:17:45 +0700

    cập nhật và xữ lý top10_chuyenmuc

--- routes/Home.route.js
+++ routes/Home.route.js
@@ -4,7 +4,7 @@ const moment = require('moment');
 const config = require('../config/config.json');
 const router = express.Router();
 
-router.get('/lien-he',(req,res) => {
+router.get('/lien-he', (req, res) => {
   res.render('about');
 })
 
@@ -17,6 +17,7 @@ router.get('/', async function (req, res) {
   const top10_chungkhoan = await articleModel.top10_chungkhoan();
   const top10_doanhnghiep = await articleModel.top10_doanhnghiep();
   const top10_taichinh = await articleModel.top10_taichinh();
+  const top10_kienthucdautu = await articleModel.top10_kienthucdautu();
   res.render('home', {
     newlist,
     bestlist1,
@@ -26,6 +27,7 @@ router.get('/', async function (req, res) {
     top10_chungkhoan,
     top10_doanhnghiep,
     top10_taichinh,
+    top10_kienthucdautu,
     helpers: {
       format_DOB: function (date) {
         //console.log(moment(date, 'YYYY/MM/DD').format('DD-MM-YYYY'));
@@ -35,17 +37,17 @@ router.get('/', async function (req, res) {
   })
 
 })
-router.post('/article/search' , async function (req, res) {
-  const key =req.body.key;
-  const k=key.split(' ').join('-'); 
-  res.redirect( '/article/search/'+k)
+router.post('/article/search', async function (req, res) {
+  const key = req.body.key;
+  const k = key.split(' ').join('-');
+  res.redirect('/article/search/' + k)
 })
 
 
 router.get('/article/search/:key', async function (req, res) {
-  const key=req.params.key;
-  const k=key.split('-').join(' ');
-  
+  const key = req.params.key;
+  const k = key.split('-').join(' ');
+
   const page = +req.query.page || 1;
   if (page < 0) page = 1;
   const offset = (page - 1) * config.pagination.limit;
@@ -53,38 +55,38 @@ router.get('/article/search/:key', async function (req, res) {
     articleModel.pageByCat(k, config.pagination.limit, offset),
     articleModel.countByCat(k)
   ]);
-  
+
   // tính số trang
   const nPages = Math.ceil(total / config.pagination.limit);
   const page_items = [];
   // duyệt số trang và  tính
   for (let i = 1; i <= nPages; i++) {
-      const item = {
-          value: i,
-          isActive: i === page
-      }
-      page_items.push(item);
+    const item = {
+      value: i,
+      isActive: i === page
+    }
+    page_items.push(item);
   }
-  var article_pre =[]; 
+  var article_pre = [];
   var index = 0;
   var index_remove = [];
-  while (index < listSearch.length){
-     if(listSearch[index].sts_id != 2 && listSearch[index].sts_id != null){
-        //listSearch.splice(index, 1);
-        if(index_remove.includes(index) == false){
-          index_remove.push(index);
-        }
+  while (index < listSearch.length) {
+    if (listSearch[index].sts_id != 2 && listSearch[index].sts_id != null) {
+      //listSearch.splice(index, 1);
+      if (index_remove.includes(index) == false) {
+        index_remove.push(index);
       }
-      
-      index++;
+    }
+
+    index++;
   }
-  index=0;
-  while (index < index_remove.length){
+  index = 0;
+  while (index < index_remove.length) {
     listSearch.splice(index, 1);
     index++;
   }
   const list = article_pre.concat(listSearch);
-  res.render('vwArticle/search',{
+  res.render('vwArticle/search', {
     list,
     page_items,
     prev_value: page - 1,
@@ -114,7 +116,7 @@ router.get('/article/search/:key', async function (req, res) {
           return t[2];
         }
       }
-  }
+    }
   });
 })
 module.exports = router;
