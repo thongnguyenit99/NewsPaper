8c3264951a0e1477d47fc09e2e692a921dbdffb8
= <=>
2020-07-22 15:33:30 +0700

    sua loi tim kiem bai viet, tai file pdf

--- routes/article.route.js
+++ routes/article.route.js
@@ -34,25 +34,17 @@ router.get('/generatepdf', async function(req, res) {
 });
 // download pdf
 router.get('/download', async function(req, res) {
-    const id = req.query.id;
-    if (req.session.authUser) {
-        if (req.session.authUser.premium == 1) {
-
-            var file = path.join(__dirname);
-            file = file.split('\\');
-            i = 0;
-            var path_file = "";
-            while (i < file.length - 1) {
-                path_file += file[i] + "\\";
-                i++;
-            }
-            path_file += `public\\pdf\\${id}.pdf`;
-
-            res.download(path_file, function(err) {});
-        }
-    } else {
-        res.json(false);
+    id= req.query.id;
+    var file = path.join(__dirname);
+    file = file.split('\\');
+    i = 0;
+    var path_file = "";
+    while (i < file.length - 1) {
+        path_file += file[i] + "\\";
+        i++;
     }
+    path_file += `public\\pdf\\${id}.pdf`;
+    res.download(path_file, `${id}.pdf`);
 })
 
 
